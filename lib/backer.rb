7330fef96a25370306ca7866d6946bc65051d194
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-19 20:48:33 +0000

    automatically backed up by learn

---
+++ lib/backer.rb
@@ -0,0 +1,14 @@
+class Backer
+
+  attr_accessor :name, :backed_projects
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+  end
+
+  def back_project(new_project = Project.new(title))
+    @backed_projects << new_project
+    
+  end
+end
---
+++ lib/project.rb
@@ -0,0 +1,12 @@
+class Project
+  attr_accessor :title, :backers
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(new_backer = Backer.new(name))
+    @backers << new_backer
+  end
+end
