17d6c4ab35bca9ca0ae7ea5de8c7abe7ecacd7ee
Leslie Stevens <flylady@mail.utexas.edu>
2018-06-19 22:54:44 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -7,11 +7,8 @@ class Project
   end
 
   def add_backer(new_backer = Backer.new(name))
-    if @backers.include?(new_backer)
-      return
-    else
-      @backer << new_backer
-      new_backer.add_project(self)
+    @backer << new_backer unless @backers.include?(new_backer)  
+    new_backer.add_project(self)
     end
 
 
